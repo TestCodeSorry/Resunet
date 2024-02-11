45e99037e4f02390dac9665b5e8271a1afcdaebe
dnbulykin@yandex.ru <dnbulykin@yandex.ru>
2024-02-11 16:41:18 +0300

    #7 00:00:00

--- Resutest/Helpers/BaseTest.cs
+++ Resutest/Helpers/BaseTest.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Resunet.BL.Auth;
 using Resunet.DAL;
-
 
 namespace Resutest.Helpers
 {
